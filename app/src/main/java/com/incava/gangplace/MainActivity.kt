9d887d3ef54e5e6695f281de3bae3297b0c0574d
incava <ingi1118@naver.com>
2023-03-27 13:54:56 +0900

    fix : 바텀 네비게이션 layout구현 id값 수정.

--- app/src/main/java/com/incava/gangplace/MainActivity.kt
+++ app/src/main/java/com/incava/gangplace/MainActivity.kt
@@ -2,10 +2,31 @@ package com.incava.gangplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.incava.gangplace.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initBinding()
+        initNavigation()
     }
+    private fun initNavigation() {
+            // NavController 획득
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+            val navController = navHostFragment.findNavController()
+            binding.bottomNav.setupWithNavController(navController)
+        }
+    private fun initBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+    }
+
 }
